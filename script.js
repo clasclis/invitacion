750c4ef4c9b2f66559c214b5ed86f218502347b8
clasclis <57096626+clasclis@users.noreply.github.com>
2024-09-24 11:54:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const card = document.querySelector('.card-inner');
 card.addEventListener('click', function() {
   card.classList.toggle('is-flipped'); // Flip on click
 });
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth <= 600) {
+    // Código para móviles
+  } else {
+    // Código para pantallas grandes
+  }
+});
+
